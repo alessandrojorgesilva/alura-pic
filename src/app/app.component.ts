e8ef6689bcaf632e3fe94b76628afb6c0c09197a
alessandrojorgesilva <alessandro.jorgers@gmail.com>
2022-06-04 17:43:25 -0300

    criando o primeiro componente e utilizando Inbound properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'alura-pic';
-  url = 'https://www.petz.com.br/blog/wp-content/uploads/2022/01/curiosidades-sobre-leao.jpg';
-  nome = 'Leão';
+export class AppComponent { 
   
 
 }
