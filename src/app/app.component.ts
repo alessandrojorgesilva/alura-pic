bdc9ccc34da5ac58760401d143ac31b17d5f32c1
alessandrojorgesilva <alessandro.jorgers@gmail.com>
2022-06-04 16:14:29 -0300

    primeiro commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'alura-pic';
+  url = 'https://www.petz.com.br/blog/wp-content/uploads/2022/01/curiosidades-sobre-leao.jpg';
+  nome = 'Leão';
+  
+
+}
