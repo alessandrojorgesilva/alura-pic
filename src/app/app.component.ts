529015abd108d2e85d1253c80802ee1ceb63c191
alessandro <alessandro.silva@d2ti.com.br>
2022-06-19 22:24:20 -0300

    criação do photo.service.ts, photo.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,27 @@
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { PhotoComponent } from './photos/photo/photo.component';
+import { Component, OnInit } from '@angular/core';
+
+import { PhotoService } from './photos/photo/photo.service';
+import { Photo } from './photos/photo/photo';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  photos: any[] = [] 
+  photos: Photo[] = [];
 
-  constructor(http: HttpClient){
-    
-    http.get<any[]>('http://localhost:3000/flavio/photos').subscribe(photos => this.photos = photos)
-    
+  constructor(private photoService: PhotoService){}
+
+  ngOnInit(){
+
+    this.photoService
+      .listFromUser('flavio')
+      .subscribe(photos =>this.photos = photos);
+
   }
-  
+
  }
