65709b9cd9ebce738001523e18235c58f0740e0b
alessandro <alessandro.silva@d2ti.com.br>
2022-06-26 16:39:53 -0300

    criando um pipe

--- src/app/photos/photos-list/photos/photos.component.ts
+++ src/app/photos/photos-list/photos/photos.component.ts
@@ -8,10 +8,10 @@ import { Photo } from '../../photo/photo';
   styleUrls: ['./photos.component.css']
 })
 export class PhotosComponent implements OnChanges {
-  
+
   @Input() photos: Photo[] = [];
   rows: any[] = [];
-  
+
   constructor() { }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -24,13 +24,13 @@ export class PhotosComponent implements OnChanges {
     this.rows = this.groupColumns(this.photos);
 }
 
-groupColumns(photos: Photo[]) {
-    const newRows = [];
+  groupColumns(photos: Photo[]) {
+      const newRows = [];
 
-    for(let index = 0; index < photos.length; index+=3) {
-        newRows.push(photos.slice(index, index + 3));
-    }
-    return newRows;
-}
+      for(let index = 0; index < photos.length; index+=3) {
+          newRows.push(photos.slice(index, index + 3));
+      }
+      return newRows;
+  }
 
 }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,3 +1,4 @@
+import { FilterByDescription } from './photos-list/photos/filter-by-description.pipe';
 
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
@@ -13,7 +14,8 @@ import { PhotosComponent } from './photos-list/photos/photos.component';
      PhotoComponent,
      PhotosListComponent,
      PhotosFormComponent,
-     PhotosComponent
+     PhotosComponent,
+     FilterByDescription
   ],
   imports: [
     HttpClientModule,
