cf9d552042ad11e2c64d9b2413a288b534cd6f8e
alessandrojorgesilva <alessandro.jorgers@gmail.com>
2022-08-01 22:42:11 -0300

    submódulos

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,28 +1,14 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-
-import { PhotoComponent } from './photo/photo.component';
-import { PhotosListComponent } from './photos-list/photos-list.component';
-import { PhotosFormComponent } from './photos-form/photos-form.component';
-import { PhotosComponent } from './photos-list/photos/photos.component';
-import { LoadButtonComponent } from './photos-list/load-button/load-button.component';
-import { FilterByDescription } from './photos-list/filter-by-description.pipe';
+import { PhotoModule } from './photo/photo.module';
+import { PhotoFormModule } from './photos-form/photo-form.module';
+import { PhotoListModule } from './photos-list/photo-list.module';
 
 
 @NgModule({
-  declarations: [
-     PhotoComponent,
-     PhotosListComponent,
-     PhotosFormComponent,
-     PhotosComponent,
-     LoadButtonComponent,
-     FilterByDescription
-
-  ],
   imports: [
-    HttpClientModule,
-    CommonModule
+    PhotoModule,
+    PhotoFormModule,
+    PhotoListModule    
   ]
 })
 
